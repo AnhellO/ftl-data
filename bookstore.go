ee613fa71369fd37c51a1309dbd94555cdaa6435
Angel Santiago Jaime Zavala <asjz2892@gmail.com>
2021-01-03 22:06:11 -0600

    feat(ftl-behaviour): finalize chapter 1

--- bookstore.go
+++ bookstore.go
@@ -1,5 +1,9 @@
 package bookstore
 
+import (
+	"fmt"
+)
+
 // Book describes a book object
 type Book struct {
 	Title           string
@@ -13,4 +17,22 @@ type Book struct {
 	SeriesName      string
 	SeriesID        int
 	Featured        bool
+}
+
+// SalePriceCents returns the sale price of a book, which is the 50% of the normal price
+func (b Book) SalePriceCents() int {
+	return b.PriceCents / 2
+}
+
+// Customer describes a customer of the bookstore
+type Customer struct {
+	Title   string
+	Name    string
+	Address string
+}
+
+// MailingLabel returns a string containing the customer’s title, name, and address,
+// suitable for printing out to a mailing label.
+func (c Customer) MailingLabel() string {
+	return fmt.Sprintf("Customer: %s %s\nAddress: %s", c.Title, c.Name, c.Address)
 }
--- bookstore_test.go
+++ bookstore_test.go
@@ -18,3 +18,28 @@ func TestBook(t *testing.T) {
 		Featured:        true,
 	}
 }
+
+func TestSalePriceCents(t *testing.T) {
+	b := bookstore.Book{
+		Title:      "A Clockwork Orange Soda",
+		PriceCents: 500,
+	}
+	want := 250
+	got := b.SalePriceCents()
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestCustomerMailingLabel(t *testing.T) {
+	c := bookstore.Customer{
+		Title:   "Mr.",
+		Name:    "A.J.",
+		Address: "Street X #123, Neighbour Hood, MX",
+	}
+	want := "Customer: Mr. A.J.\nAddress: Street X #123, Neighbour Hood, MX"
+	got := c.MailingLabel()
+	if want != got {
+		t.Errorf("want %s, got %s", want, got)
+	}
+}
